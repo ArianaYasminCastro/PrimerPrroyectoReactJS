d66c8a6cd30f27077c7f9f6bd904529bfa5878a6
Ariana Castro <ariibritez5@gmail.com>
2023-11-01 16:55:53 -0300

    agregue una navbar con boostrap

--- proyecto-ariana-castro/src/App.jsx
+++ proyecto-ariana-castro/src/App.jsx
@@ -1,6 +1,6 @@
 import { ItemListCotainer } from "./components/ItemListContainer/ItemListCotainer"
 import { Navbar } from "./components/Navbar/Navbar"
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App(){
   return(
     <>
--- proyecto-ariana-castro/src/components/CartWidget/CartWidget.jsx
+++ proyecto-ariana-castro/src/components/CartWidget/CartWidget.jsx
@@ -1,7 +1,7 @@
 export const CartWidget = () => {
   return (
     <div>
-       
+       <img src="https://www.xenonfactory.es/wp-content/uploads/2019/01/carrito-compras-desarrollo-tienda-virtual.png" alt="carrrito" />
     </div>
   )
 }
